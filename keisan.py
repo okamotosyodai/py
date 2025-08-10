dea52e1621780d1eb1c4c632475ca24bd591a207
okamotosyodai <c1417393@st.kanazawa-it.ac.jp>
2025-08-11 00:31:47 +0900

    Add hello.py

---
+++ keisan.py
@@ -0,0 +1,27 @@
+print(2 + 3)
+print(3 + 6 / 3)
+print((3 + 6) / 3)
+a = 10
+print(a + 3)
+a = 10
+a = a + 3 #変数aに3を加えた値を代入する
+print(a)
+a = 10
+a += 3
+a = 12
+a *= 2
+print(a)
+a %= 5
+print(a)
+print(type(3 + 2))
+print(type(3 - 2))
+print(type(3 * 2))
+print(type(3 / 2))
+print(type(4 / 2))
+a = float(1)
+print(a)
+type(a)
+a = int(1.9)
+print(a)
+a = 1.2 + 3
+print(a)
